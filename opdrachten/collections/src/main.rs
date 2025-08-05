2c13698fb70f4d24f3e4f8e657d71551293ab266
Kevin Buijsers <buijsersschool@gmail.com>
2025-08-05 14:15:58 +0200

    Fixing some cargo

--- opdrachten/collections/src/main.rs
+++ opdrachten/collections/src/main.rs
@@ -8,7 +8,10 @@ fn main() {
     let v = vec![1, 2, 3, 4, 5];
 
     let third = &v[2];
-    println!("The third element is {third}")
+    println!("The third element is {third}");
 
-    
+    match v.get(2){
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
 }
