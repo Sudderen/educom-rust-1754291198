fbae3eba7a5f9f95f2d7e5f4e38627c1a9e97321
Kevin Buijsers <buijsersschool@gmail.com>
2025-09-02 10:05:54 +0200

    Added duplicate protection

--- case/pokeAPI/src/main.rs
+++ case/pokeAPI/src/main.rs
@@ -18,20 +18,28 @@ pub struct Pokemon {
     pokedex: i64,
 }
 
+#[derive(Debug, FromRow)]
+pub struct Type {
+    r#type: String
+}
+
 fn main() {
     let name = get_pokemon_name();
-    let fetch: Value = match fetch_pokemon(name.clone()) {
+    let Ok(_) = get_pokemon_from_db(name.clone()) else {
+        // get_pokemon_from_ai(&name);
+        let fetch: Value = match fetch_pokemon(name.clone()) {
         Ok(val) => val,
         Err(err) => {
             println!("Error: {}", err);
             return;
         }
+        };
+        let (pokemon, r#type) = get_pokemon_information(fetch);
+        let _ = add_pokemon_to_db(pokemon, r#type);
+        let _ = get_pokemon_from_db(name);
+        return 
     };
-    let pokemon_information = get_pokemon_information(fetch);
-    let _ = add_pokemon_to_db(pokemon_information);
-    let _ = get_pokemon_from_db(name);
 }
-
 
 fn fetch_pokemon(name: String) -> Result<Value, String>{
     let url  = format!("https://pokeapi.co/api/v2/pokemon/{}", name);
@@ -47,7 +55,8 @@ fn fetch_pokemon(name: String) -> Result<Value, String>{
     Ok(data)
 }
 
-fn add_pokemon_to_db(json: Pokemon) -> Result<(), Box<dyn Error>>{
+fn add_pokemon_to_db(json: Pokemon, r#type: Vec<String>) -> Result<(), Box<dyn Error>>{
+
     let settings = config::Config::builder()
         .add_source(config::File::with_name("Config"))
         .build()
@@ -64,7 +73,9 @@ fn add_pokemon_to_db(json: Pokemon) -> Result<(), Box<dyn Error>>{
         .get_conn()
         .map_err(|e| format!("Failed to get DB connection: {}", e))?;
 
-    conn.exec_drop(
+    let mut tx = conn.start_transaction(TxOpts::default())?;
+
+    tx.exec_drop(
         r"INSERT INTO pokemon (name, height, weight, pokedex) VALUES (:name, :height, :weight, :pokedex)",
         params! {
             "name" => &json.name,
@@ -74,6 +85,32 @@ fn add_pokemon_to_db(json: Pokemon) -> Result<(), Box<dyn Error>>{
         },
     )?;
 
+    let pokemon_id = tx.last_insert_id();
+
+    for type_name in &r#type {
+        let maybe_type_id:Option<u64> = tx.exec_first(
+            "SELECT id FROM types WHERE type = :type",
+            params! {
+                "type" => type_name
+            },)?;
+        let type_id = if let Some(id) = maybe_type_id {
+            id
+        } else {
+            tx.exec_drop(
+                "INSERT INTO types (type) VALUES (:type)",
+                params! {
+                    "type" => type_name
+                },)?;
+                tx.last_insert_id().unwrap()
+        };
+        tx.exec_drop(
+            "INSERT INTO pokemon_types (pokemon_id, type_id) VALUES (:pokemon_id, :type_id)",
+            params! {
+                "pokemon_id" => pokemon_id,
+                "type_id" => type_id,
+            },)?;
+    }
+    tx.commit()?;
     Ok(())
 }
 
@@ -92,23 +129,44 @@ async fn get_pokemon_from_db(name: String) -> Result<(), Box<dyn Error>> {
         .connect(&database_url)
         .await
         .map_err(|e| format!("Failed to connect to DB: {}", e))?;
-    
 
-    let result = sqlx::query_as::<_, Pokemon>(
-        "SELECT name, height, weight, pokedex FROM pokemon WHERE name = ?"
+    let pokemon = sqlx::query_as::<_, Pokemon>(
+        "SELECT id, name, height, weight, pokedex FROM pokemon WHERE name = ?"
     )
     .bind(&name)
     .fetch_one(&pool)
-    .await;
+    .await
+    .map_err(|e| format!("Query failed: {}", e))?;
 
+    let types  = sqlx::query_as::<_, Type>(
+        r#"
+        SELECT t.type 
+        FROM types t
+        JOIN pokemon_types pt ON pt.type_id = t.id
+        JOIN pokemon p ON p.id = pt.pokemon_id
+        WHERE p.name = ?
+        ORDER BY t.id
+        "#
+    )
+    .bind(&name)
+    .fetch_all(&pool)
+    .await
+    .map_err(|e| format!("Query failed: {}", e))?;
 
+    let poke_types = types.iter().map(|t| &t.r#type).collect::<Vec<_>>();
 
-    match result {
-        Ok(pokemon) => 
-        println!("Found pokemon: {} with height {} and weight {} and pokedex {}", pokemon.name, pokemon.height, pokemon.weight, pokemon.pokedex),
-        Err(sqlx::Error::RowNotFound) => println!("No pokemon named {} found", name),
-        Err(e) => return Err(format!("Query failed: {}", e).into())
-    };
+    println!("The pokemon is {}", pokemon.name);
+    println!("{}'s height is {}", pokemon.name, pokemon.height);
+    println!("{}'s weight is {}", pokemon.name, pokemon.weight);
+    println!("{}'s pokedex number is {}", pokemon.name, pokemon.pokedex);
+    for (i, poke_type) in poke_types.iter().enumerate() {
+        if i == 0 {
+            print!("{}'s type is {} ", pokemon.name, poke_type)
+        }
+        if i ==1 {
+            print!("and {}", poke_type)
+        }
+    }
    
     Ok(())
 }
@@ -121,13 +179,20 @@ async fn get_pokemon_from_db(name: String) -> Result<(), Box<dyn Error>> {
 //     Ok(contents)
 // }
 
-fn get_pokemon_information(string_data: Value) -> Pokemon {
+fn get_pokemon_information(string_data: Value) -> (Pokemon, Vec<String>) {
     let mut pokemon_name: Option<String> = None;
     let mut pokemon_dex: Option<i64> = None;
+    let mut pokemon_type: Vec<String> = vec![];
     if let Some(forms) = string_data["forms"].as_array() {
         for form in forms {
             let name = form["name"].as_str().unwrap();
             pokemon_name = Some(name.to_string());
+        }
+    }
+    if let Some(types) = string_data["types"].as_array() {
+        for r#type in types {
+            let poke_type = r#type["type"]["name"].as_str().unwrap();
+            pokemon_type.push(poke_type.to_string());
         }
     }
     let pokemon_height= string_data["height"].as_i64().unwrap();
@@ -140,11 +205,12 @@ fn get_pokemon_information(string_data: Value) -> Pokemon {
     }
     let pokemon = Pokemon {
         name: pokemon_name.unwrap(),
-        height: pokemon_height,
+        height: pokemon_height, 
         weight: pokemon_weight,
         pokedex: pokemon_dex.unwrap()
     };
-    pokemon
+
+    (pokemon, pokemon_type)
 }
 
 fn get_pokemon_name() -> String {
@@ -158,3 +224,7 @@ fn get_pokemon_name() -> String {
     input.pop();
     String::from(input)
 }
+
+// fn get_pokemon_from_ai(name: &str){
+
+// }
